60e16490a70ee99245aafd59c50ef3e4e58bb5d1
rawat9015 <nr7584128@gmail.com>
2025-02-28 23:37:39 +0530

    starting with npm

--- react.js
+++ react.js
@@ -1,8 +1,15 @@
 
 const heading = React.createElement("h1" , {id:'head'}, "Hello World from React!");
 
+const parentdiv = React.createElement("div" , {id:'parentdiv'}, 
+    [React.createElement('div',{id:'childdiv'}, 
+        [React.createElement('p', {id:'para1'},'this is para 1')]
+    )]
+);
+
+
 const img = React.createElement("img", {src:'img1.jpeg', style:{width:'250px',height:'200px'}});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(img);
+root.render([img,heading,parentdiv]);
